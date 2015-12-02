ed449afec006f42fec37b05b2e8c6c5a3ce5cbd8
Vsevolod Herasymenko <sysmet@meta.ua>
2015-12-02 16:08:50 +0200

    changes

--- src/main/java/GVA/LibraryOnline/WebConfiguration.java
+++ src/main/java/GVA/LibraryOnline/WebConfiguration.java
@@ -1,24 +1,16 @@
 package GVA.LibraryOnline;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @org.springframework.context.annotation.Configuration
 @EnableAutoConfiguration
-@EnableWebMvc
 public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/web/weblib/**").addResourceLocations(
                 "/WEB-INF/weblib/");
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
     }
 }
