0df8edca4fd3ac17e706258868b6ab2ed7b504b5
Vsevolod Herasymenko <sysmet@meta.ua>
2015-12-02 16:55:00 +0200

    spring boot ver 1.2.7

--- src/main/java/GVA/LibraryOnline/WebApplication.java
+++ src/main/java/GVA/LibraryOnline/WebApplication.java
@@ -1,7 +1,6 @@
 package GVA.LibraryOnline;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
