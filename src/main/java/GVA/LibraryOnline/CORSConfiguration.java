0e3095ad3ec0634c717dfcc984eaec9db0a9f3c9
Vsevolod Herasymenko <sysmet@meta.ua>
2015-12-02 15:45:20 +0200

    changes

--- src/main/java/GVA/LibraryOnline/CORSConfiguration.java
+++ src/main/java/GVA/LibraryOnline/CORSConfiguration.java
@@ -1,5 +1,6 @@
 package GVA.LibraryOnline;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * Created by V.Herasymenko on 02.12.2015.
  */
 @Configuration
+@EnableAutoConfiguration
 public class CORSConfiguration {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
--- src/main/java/GVA/LibraryOnline/WebApplication.java
+++ src/main/java/GVA/LibraryOnline/WebApplication.java
@@ -1,6 +1,7 @@
 package GVA.LibraryOnline;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
@@ -9,6 +10,7 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
  * Created by V.Herasymenko on 05.10.2015.
  */
 @SpringBootApplication
+@EnableAutoConfiguration
 public class WebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
