77d89f501734c59b40101c7d67db29124912020c
Vsevolod Herasymenko <sysmet@meta.ua>
2015-12-03 17:07:53 +0200

    fix

--- src/main/webapp/WEB-INF/weblib/js/getBooks.js
+++ src/main/webapp/WEB-INF/weblib/js/getBooks.js
@@ -17,7 +17,7 @@ function printBooks(){
 	var books = getBooks();
 	var block = $("#books");
 	for (var i = 0; i <= books.length; i++){
-		var string = "<div>" + books[i].name + "/div";
+		var string = "<div>" + books[i].name + "</div>";
 		$(block).add(string);
 	}
 }
