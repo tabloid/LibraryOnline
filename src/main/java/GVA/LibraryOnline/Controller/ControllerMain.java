ddb5ba0594938839eeccb39355506857bcfd9742
v.herasymenko <sysmet@meta.ua>
2016-02-18 17:16:00 +0200

    redirect added

---
+++ src/main/java/GVA/LibraryOnline/Controller/ControllerMain.java
@@ -0,0 +1,19 @@
+package GVA.LibraryOnline.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by v.herasymenko on 2/18/2016.
+ */
+@Controller
+public class ControllerMain {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView getIndex() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("redirect:/web/client");
+        return mav;
+    }
+}
